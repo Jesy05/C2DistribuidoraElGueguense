4c7ce12ebea48532646af94c52f37606097d858a
Jesy05 <jgonzalezj@uamv.edu.ni>
2026-09-02 15:34:27 -0600

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ni.edu.uam.c2distribuidoraelgueguense {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ni.edu.uam.c2distribuidoraelgueguense to javafx.fxml;
+    exports ni.edu.uam.c2distribuidoraelgueguense;
+}
---
+++ src/main/java/ni/edu/uam/c2distribuidoraelgueguense/Launcher.java
@@ -0,0 +1,9 @@
+package ni.edu.uam.c2distribuidoraelgueguense;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
